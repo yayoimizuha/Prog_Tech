37a80b07948c43d12d0241936eaf97f5a4da7177
yayoimizuha <poko.peko.tomo@icloud.com>
2022-04-10 16:02:15 +0900

    remove garbage

--- komachi.c
+++ komachi.c
@@ -1,12 +1,6 @@
-//
-// Created by tomokazu on 2022/04/08.
-//
-
-#include <stdlib.h>
 #include <malloc.h>
-#include <string.h>
-#include <stdbool.h>
 #include <math.h>
+#include <stdio.h>
 
 #define LEN 5000
 #define MATCH_NUM 237
@@ -17,8 +11,7 @@ int main(void) {
 
     char *process_string = calloc(LEN, sizeof(char));
     int *num_arr = malloc(sizeof(int) * LEN / 2);
-    memset(num_arr, 0, sizeof(int) * LEN / 2);
-    process_string = "34 35 22 5 23 65 54 76 92 46 26 32 17 16 151 5 18";
+    process_string = "34 35 22 5 23 65 54 0 0   76 92 46 26 32 17 16 151 5 18";
     int num = 0;
     int num_arr_order = 0;
     for (int i = 0; i < LEN; ++i) {
@@ -44,11 +37,10 @@ int main(void) {
         if (num_arr[i] == 0)break;
         array_length++;
     }
-    printf("\n\t%d\n\n", array_length);
+    printf("\n\t数の数: %d\n\n", array_length);
     int *num_array = (int *) malloc(array_length * sizeof(int));
     for (int i = 0; i < array_length; ++i) {
         num_array[i] = num_arr[i];
-        printf("\n%d", num_array[i]);
     }
 
     printf("\n\n");
@@ -60,23 +52,11 @@ int main(void) {
     for (int i = 0; i < (int) pow(2, array_length); ++i) {
         k = i;
         num_sum = 0;
-        //printf("%d\n", (int) k);
-        //printf("\n");
         for (int j = 0; j < array_length; ++j) {
             bin_num = (int) (k / pow(2, array_length - j - 1));
-            //printf("%d ", bin_num);
-            if (bin_num == 1) {
-                //printf("-");
-            } else {
-                //printf("+");
-            }
-            //printf("%d", num_array[j]);
-
             num_sum += num_array[j] * (int) pow(-1, bin_num);
             k -= bin_num * (int) pow(2, array_length - j - 1);
         }
-        //printf("\n");
-        //printf("num_sum: %d\n", num_sum);
         if (MATCH_NUM == num_sum) {
             match_num[matches] = i;
             matches++;
@@ -90,7 +70,6 @@ int main(void) {
         k = match_num[i];
         for (int j = 0; j < array_length; ++j) {
             bin_num = (int) (k / pow(2, array_length - j - 1));
-            //printf("%d ", bin_num);
             if (bin_num == 1) {
                 printf("-");
             } else {
@@ -102,12 +81,8 @@ int main(void) {
             k -= bin_num * (int) pow(2, array_length - j - 1);
         }
         printf("=%d\n", MATCH_NUM);
-
     }
-
-
 }
-
 
 int char_to_int(char C) {
     int tmp = C - 48;
