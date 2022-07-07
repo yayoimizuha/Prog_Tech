f4f8e37573c1c9ea85dd7a351643e6facc62dd79
yayoimizuha <poko.peko.tomo@icloud.com>
2022-07-07 18:15:31 +0900

    変更

--- rand.c
+++ rand.c
@@ -4,10 +4,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-
-#define NUM    1e+4    //逕滓�先焚
+#define NUM    1e+4    //生成数
 #define A 9
 #define C 13
 #define M 4096
@@ -22,8 +20,16 @@ unsigned int lcgs(unsigned int x) {
 
 int main(int argc, char *argv[]) {
     FILE *fp1, *gnuplot;
-    int i;
+
     unsigned long x, y;
+
+    typedef struct {
+        unsigned long x;
+        unsigned long y;
+    } xy;
+
+    xy *rand_arr = malloc(sizeof(xy) * NUM);
+    int *rand_hist = calloc(M, sizeof(int));
 
     // Usage
     if (argc != 2) {
@@ -37,24 +43,41 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    gnuplot = popen(GNUPLOT_PATH, "w");
-
+    if ((gnuplot = popen(GNUPLOT_PATH, "w")) == NULL) {
+        fprintf(stderr, "Cannot execute gnuplot. Please check file location.\n");
+        fprintf(stderr, GNUPLOT_PATH);
+        exit(1);
+    }
     fprintf(gnuplot, "set terminal windows color\n");
+    fprintf(gnuplot, "set title '線形合同法'\n");
     fprintf(gnuplot, "set grid\n");
+    fprintf(gnuplot, "set y2tics\n");
+    fprintf(gnuplot, "set y2range [-2:9]\n");
+    fprintf(gnuplot, "set ylabel '乱数の分布(散布図)'\n");
+    fprintf(gnuplot, "set y2label '乱数の分布(折れ線)'\n");
     fprintf(gnuplot, "set size square\n");
-    fprintf(gnuplot, "plot [-4000:37000] [-4000:37000] '-' using 2:3 with lp pointtype 7 linecolor 'turquoise'\n");
+    fprintf(gnuplot,
+            "plot [-500:4500] [-500:4500] '-' using 2:3 axis x1y1 w l linecolor 'pink' title 'line',"
+            " '-' using 2:3 axis x1y1 w p linecolor 'red' pointsize 0.2 pointtype 7 title 'point' ,"
+            " '-' using 1:2 axis x1y2 w l lc rgb 0x440000ff title 'hist' \n");
     x = 0;
     y = 1;
-    for (i = 0; i < NUM; i++) {
-        x = lcgs(x);
-        y = lcgs(y);
-        // x = rand();
-        // y = rand();
+    for (int i = 0; i < NUM; i++) {
+        rand_arr[i].x = x = lcgs(x);
+        rand_hist[x]++;
+        rand_arr[i].y = y = lcgs(y);
         fprintf(fp1, "%u\t%u\n", x, y);
         fprintf(gnuplot, "%d\t%u\t%u\n", i, x, y);
-
     }
+    fprintf(gnuplot, "e\n");
 
+    for (int i = 0; i < NUM; ++i) {
+        fprintf(gnuplot, "%d\t%u\t%u\n", i, rand_arr[i].x, rand_arr[i].y);
+    }
+    fprintf(gnuplot, "e\n");
+    for (int i = 0; i < M; ++i) {
+        fprintf(gnuplot, "%d\t%d\n", i, rand_hist[i]);
+    }
     fprintf(gnuplot, "e\n");
     pclose(gnuplot);
 
--- rand_rand.c
+++ rand_rand.c
@@ -5,22 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM    1e+4    //ç”Ÿæˆæ•°
-#define A 9
-#define C 13
-#define M 4096
-
-unsigned int lcgs(unsigned int x) {
-    unsigned int y;
-    y = (A * x + C) % M;
-    return y;
-}
+#define NUM    1e+4    //¶¬”
+#define GNUPLOT_PATH "C:\\Progra~1\\gnuplot\\bin\\gnuplot.exe -persist"
 
 int main(int argc, char *argv[]) {
-    FILE *fp1;
-    int i;
-    unsigned int x, y;
-    printf("%d\n", argc);
+    FILE *fp1, *gnuplot;
+
+    unsigned long x, y;
+
+    typedef struct {
+        unsigned long x;
+        unsigned long y;
+    } xy;
+
+    xy *rand_arr = malloc(sizeof(xy) * NUM);
+    int *rand_hist = calloc(RAND_MAX, sizeof(int));
 
     // Usage
     if (argc != 2) {
@@ -34,14 +33,45 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    if ((gnuplot = popen(GNUPLOT_PATH, "w")) == NULL) {
+        fprintf(stderr, "Cannot execute gnuplot. Please check file location.\n");
+        fprintf(stderr, GNUPLOT_PATH);
+        exit(1);
+    }
+    fprintf(gnuplot, "set terminal windows color\n");
+    fprintf(gnuplot, "set title 'rand()ŠÖ”'\n");
+    fprintf(gnuplot, "set grid\n");
+    fprintf(gnuplot, "set y2tics\n");
+    fprintf(gnuplot, "set y2range [-2:9]\n");
+    fprintf(gnuplot, "set ylabel '—”‚Ì•ª•z(U•z})'\n");
+    fprintf(gnuplot, "set y2label '—”‚Ì•ª•z(Ü‚êü)'\n");
+    fprintf(gnuplot, "set size square\n");
+    fprintf(gnuplot,
+            "plot [-4000:36767] [-4000:36767] '-' using 2:3 axis x1y1 w l linecolor 'pink' title 'line',"
+            " '-' using 2:3 axis x1y1 w p linecolor 'red' pointsize 0.2 pointtype 7 title 'point' ,"
+            " '-' using 1:2 axis x1y2 w l lc rgb 0x440000ff title 'hist' \n");
     x = 0;
     y = 1;
-    for (i = 0; i < NUM; i++) {
-        x = rand();
-        y = rand();
-        fprintf(fp1, "%d %u %u\n", i, x, y);
-    }
+    for (int i = 0; i < NUM; i++) {
+        rand_arr[i].x = x = rand();
+        rand_hist[x]++;
+        rand_arr[i].y = y = rand();
+        fprintf(fp1, "%u\t%u\n", x, y);
+        fprintf(gnuplot, "%d\t%u\t%u\n", i, x, y);
 
+    }
+    fprintf(gnuplot, "e\n");
+
+    for (int i = 0; i < NUM; ++i) {
+        fprintf(gnuplot, "%d\t%u\t%u\n", i, rand_arr[i].x, rand_arr[i].y);
+    }
+    fprintf(gnuplot, "e\n");
+    for (int i = 0; i < RAND_MAX; ++i) {
+        // printf("%d\n", rand_hist[i]);
+        fprintf(gnuplot, "%d\t%d\n", i, rand_hist[i]);
+    }
+    fprintf(gnuplot, "e\n");
+    pclose(gnuplot);
 
     return 0;
 }
